41989dcfdeafd6ede80e4f85910642cefa3a1dd3
Theo POTILLION <tpotilli@bess-f4r1s14.clusters.42paris.fr>
2023-10-04 11:00:49 +0200

    o

--- srcs/ft_parent_process.c
+++ srcs/ft_parent_process.c
@@ -6,7 +6,7 @@
 /*   By: tpotilli <tpotilli@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/11 15:59:09 by tpotilli          #+#    #+#             */
-/*   Updated: 2023/10/04 10:52:05 by tpotilli         ###   ########.fr       */
+/*   Updated: 2023/10/04 10:56:43 by tpotilli         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,8 @@
 int parent_process(int fd4, char *argv[], char *envp[], int *end)
 {
    close(end[1]);
-    /*if (dup2 < 0)
-		return (1);*/
-	execve(argv[1], argv, envp);
-	dup2(fd1, end[0]);
+	execve(argv[4], argv, envp);
+	dup2(fd4, end[0]);
     close(end[0]);
 	close(fd4);
 	exit(EXIT_SUCCESS);
